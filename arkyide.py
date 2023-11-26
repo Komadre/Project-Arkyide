bdc862a5c23a58e588d47325dc4a46f07c9dc699
Komadre <141841736+Komadre@users.noreply.github.com>
2023-11-26 16:45:08 +0100

    update arkyide.py

--- arkyide.py
+++ arkyide.py
@@ -1,7 +1,12 @@
 import os 
 import git
 import pyfiglet
+import random
 #Work in progress
 # function wmm = welcome message and menu, for later
 def wmm():
     wmm = pyfiglet.figlet_format("Arkyide")
+def wmm2():
+
+def wmm3():
+    
