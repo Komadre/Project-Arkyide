1536d5ce2dc11255f2f4e4c08be062b2a7c73906
Komadre <141841736+Komadre@users.noreply.github.com>
2023-11-27 16:18:01 +0100

    update arkyide.py

--- arkyide.py
+++ arkyide.py
@@ -6,15 +6,4 @@ import random
 # function wmm = welcome message and menu, for later
 def wmm():
     wmm = pyfiglet.figlet_format("Arkyide")
-def wmm2():
-
-def wmm3()
-
-
-randomwmm = [wmm, wmm2, wmm3]
-
-randomwmmprint = random.choice(randomwmm)
-
-randomwmmtext = randomwmmprint()
-
-print(randomwmmtext)
+def 
